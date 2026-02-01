3ca50db36796f51357bddf7380b573c4a77c75be
Aizat <a1izekaa18@gmail.com>
2026-02-01 23:55:45 +0500

    Initial project structure

--- internal/pet/model.go
+++ internal/pet/model.go
@@ -4,7 +4,7 @@ type Pet struct {
 	ID      int     `json:"id"`
 	Name    string  `json:"name"`
 	Species string  `json:"species"`
-	Age     int     `json:"age"`
+	Age     int     `jsn:"age"`
 	Price   float64 `json:"price"`
 	Status  string  `json:"status"`
 }
