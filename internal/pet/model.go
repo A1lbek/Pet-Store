65cf7c500d2f7db8a8652f4d7fe57f16319246aa
Akbota <240938@astanait.edu.kz>
2026-02-01 23:56:36 +0500

    Initial project structure

--- internal/pet/model.go
+++ internal/pet/model.go
@@ -4,7 +4,7 @@ type Pet struct {
 	ID      int     `json:"id"`
 	Name    string  `json:"name"`
 	Species string  `json:"species"`
-	Age     int     `jsn:"age"`
+	Age     int     `json:"age"`
 	Price   float64 `json:"price"`
 	Status  string  `json:"status"`
 }
